97e544dda4036e3d0739ccfe6bacd7a1d7e991b4
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2024-05-22 18:06:05 +0300

    "email feature"

--- src/pages/services/apiEmail.js
+++ src/pages/services/apiEmail.js
@@ -2,8 +2,8 @@ import emailjs from '@emailjs/browser';
 import { CONTACT_SERVICE_ID, CONTACT_TEMPLATE_ID, PUBLIC_KEY } from './email';
 import toast from 'react-hot-toast';
 
-export async function sendEmail({form, action}) {
-  emailjs.send(CONTACT_SERVICE_ID, CONTACT_TEMPLATE_ID, form, PUBLIC_KEY).then(
+export async function sendEmail({data, action}) {
+  emailjs.send(CONTACT_SERVICE_ID, CONTACT_TEMPLATE_ID, data, PUBLIC_KEY).then(
     () => {
     toast.success("تم الإرسال .. شكرا لكم :)");
     action();
